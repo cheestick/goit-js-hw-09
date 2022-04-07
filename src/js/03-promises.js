9d43abb652e7d1a57c27d349ad2b623125033126
Maksym Chystiakov <maksym.chystiakov@gmail.com>
2022-04-07 11:03:39 +0300

    03-promises started

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,3 +1,12 @@
+import { Notify } from 'notiflix';
+
+const refs = {
+  form: document.querySelector('.form'),
+  delay: document.querySelector('input[name="delay"]'),
+  step: document.querySelector('input[name="step"]'),
+  amout: document.querySelector('input[name="amount"]'),
+};
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
   if (shouldResolve) {
