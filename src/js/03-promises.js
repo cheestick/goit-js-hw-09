2ca9e7b7ae6b727ba1321b233bf5d8fe867bc4b7
Maksym Chystiakov <maksym.chystiakov@gmail.com>
2022-04-07 11:58:49 +0300

    dances with promises

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -4,14 +4,37 @@ const refs = {
   form: document.querySelector('.form'),
   delay: document.querySelector('input[name="delay"]'),
   step: document.querySelector('input[name="step"]'),
-  amout: document.querySelector('input[name="amount"]'),
+  amount: document.querySelector('input[name="amount"]'),
 };
 
+refs.form.addEventListener('submit', onFormSubmit);
+
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+  return new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+
+    if (shouldResolve) {
+      // Fulfill
+      resolve({ position, delay });
+    } else {
+      // Reject
+      reject({ position, delay });
+    }
+  });
+}
+
+function onFormSubmit(e) {
+  e.preventDefault();
+  const { delay, step, amount } = e.target.elements;
+
+  for (let i = 1; i <= amount.value; i += 1) {
+    createPromise(i, delay.value)
+      .then(({ position, delay }) => {
+        Notify.success(`Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        Notify.failure(`Rejected promise ${position} in ${delay}ms`);
+      });
   }
+  e.target.reset();
 }
